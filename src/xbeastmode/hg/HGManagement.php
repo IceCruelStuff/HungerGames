62fd1afea6870381d07ed8070fb9707ab47f74e3
xBeastMode <xbeastmode00@gmail.com>
2016-03-07 12:01:46 -0800

    Create HGManagement.php

---
+++ src/xbeastmode/hg/HGManagement.php
@@ -0,0 +1,10 @@
+<?php
+namespace xbeastmode\hg;
+class HGManagement{
+    /** @var array */
+    public static $games = [];
+    /** @var array */
+    public static $players = [];
+    /** @var array */
+    public static $data = [];
+}
