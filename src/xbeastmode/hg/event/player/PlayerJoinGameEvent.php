e17c5cea91ae167f0b37bb40f8d663cc4305ef3d
xBeastMode <xbeastmode00@gmail.com>
2016-03-09 14:56:58 -0800

    Create PlayerJoinGameEvent.php

---
+++ src/xbeastmode/hg/event/player/PlayerJoinGameEvent.php
@@ -0,0 +1,47 @@
+<?php
+namespace xbeastmode\hg\event\player;
+use pocketmine\event\Cancellable;
+use pocketmine\Player;
+use xbeastmode\hg\event\HGEvent;
+use xbeastmode\hg\Loader;
+class PlayerJoinGameEvent extends HGEvent implements Cancellable{
+    public static $handlerList = null;
+
+    /** @var Player */
+    private $player;
+    private $game;
+    /**
+     * @param Loader $main
+     * @param Player $p
+     * @param $game
+     */
+    public function __construct(Loader $main, Player $p, $game){
+        parent::__construct($main);
+        $this->player = $p;
+        $this->game = $game;
+    }
+    /**
+     * @return Player
+     */
+    public function getPlayer(){
+        return $this->player;
+    }
+    /**
+     * @param Player $p
+     */
+    public function setPlayer(Player $p){
+        $this->player = $p;
+    }
+    /**
+     * @return string
+     */
+    public function getGame(){
+        return $this->game;
+    }
+    /**
+     * @param $game
+     */
+    public function setGame($game){
+        $this->game = $game;
+    }
+}
