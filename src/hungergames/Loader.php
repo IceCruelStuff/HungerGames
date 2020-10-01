5fb10b3b000139f5c865f5886c85b3f7dba8a141
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2020-10-01 13:44:54 -0700

    Update Loader.php

--- src/hungergames/Loader.php
+++ src/hungergames/Loader.php
@@ -1,5 +1,7 @@
 <?php
+
 namespace hungergames;
+
 use hungergames\api\scripts\HGAPIScriptManager;
 use hungergames\command\HGCommand;
 use hungergames\hgmap\MapBackup;
@@ -13,334 +15,330 @@ use pocketmine\utils\Config;
 use hungergames\obj\HungerGames;
 use hungergames\lib\utils\Msg;
 use hungergames\tasks\RefreshSignsTask;
-class Loader extends PluginBase{
-        /** @var Loader */
-        private static $instance = null;
 
-        /** @var GameStorage */
-        private $storage;
-        /** @var GlobalManager */
-        private $globalManager;
-        /** @var Config */
-        private $messages;
-        /** @var HGAPIScriptManager */
-        private $scriptManager;
-        /** @var MapBackup */
-        private $mapBackup;
-        /** @var SignManager */
-        private $signManager;
+class Loader extends PluginBase {
 
-        public function onLoad(){
-                while(!self::$instance instanceof $this){
-                        self::$instance = $this;
-                }
-        }
+	/** @var Loader */
+	private static $instance = null;
 
-        public function onEnable(){
-                $this->storage = new GameStorage();
-                $this->globalManager = new GlobalManager($this);
-                $this->scriptManager = new HGAPIScriptManager($this);
-                $this->mapBackup = new MapBackup($this);
-                $this->signManager = new SignManager($this);
-                $this->getServer()->getCommandMap()->register("hg", new HGCommand($this));
-                $this->getScheduler()->scheduleDelayedTask(new LoadGamesTask($this), 20);
-                $h = $this->getScheduler()->scheduleDelayedRepeatingTask($t = new RefreshSignsTask($this), 20 * 5, 20);
-                $t->setHandler($h);
-                $this->getServer()->getPluginManager()->registerEvents(new EventListener($this), $this);
-                @mkdir($this->dataPath());
-                @mkdir($this->dataPath() . "arenas/");
-                @mkdir($this->dataPath() . "resources/");
-                @mkdir($this->dataPath() . "scripts/");
-                @mkdir($this->dataPath() . "scriptConfigs/");
-                @mkdir($this->dataPath() . "mapBackups/");
-                $this->messages = new Config($this->dataPath() . "messages.yml", Config::YAML, Msg::getDefaultHGMessages());
-                $this->scriptManager->loadScripts();
-        }
+	/** @var GameStorage */
+	private $storage;
+	/** @var GlobalManager */
+	private $globalManager;
+	/** @var Config */
+	private $messages;
+	/** @var HGAPIScriptManager */
+	private $scriptManager;
+	/** @var MapBackup */
+	private $mapBackup;
+	/** @var SignManager */
+	private $signManager;
 
-        /**
-         * HungerGames base class
-         *
-         * @return Loader
-         */
-        public static function getInstance(){
-                return self::$instance;
-        }
+	public function onLoad() : void {
+		while (!self::$instance instanceof $this) {
+			self::$instance = $this;
+		}
+	}
 
-        /**
-         * HungerGames base folder
-         *
-         * @return string
-         */
-        public function dataPath(){
-                return $this->getDataFolder();
-        }
+	public function onEnable() : void {
+		$this->storage = new GameStorage();
+		$this->globalManager = new GlobalManager($this);
+		$this->scriptManager = new HGAPIScriptManager($this);
+		$this->mapBackup = new MapBackup($this);
+		$this->signManager = new SignManager($this);
+		$this->getServer()->getCommandMap()->register("hg", new HGCommand($this));
+		$this->getScheduler()->scheduleDelayedTask(new LoadGamesTask($this), 20);
+		$h = $this->getScheduler()->scheduleDelayedRepeatingTask($t = new RefreshSignsTask($this), 20 * 5, 20);
+		$t->setHandler($h);
+		$this->getServer()->getPluginManager()->registerEvents(new EventListener($this), $this);
+		@mkdir($this->dataPath());
+		@mkdir($this->dataPath() . "arenas/");
+		@mkdir($this->dataPath() . "resources/");
+		@mkdir($this->dataPath() . "scripts/");
+		@mkdir($this->dataPath() . "scriptConfigs/");
+		@mkdir($this->dataPath() . "mapBackups/");
+		$this->messages = new Config($this->dataPath() . "messages.yml", Config::YAML, Msg::getDefaultHGMessages());
+		$this->scriptManager->loadScripts();
+	}
 
-        /**
-         * HungerGames arena folder
-         *
-         * @return string
-         */
-        public function getArenasDataPath(){
-                return $this->dataPath() . "arenas/";
-        }
+	/**
+	 * HungerGames base class
+	 *
+	 * @return Loader
+	 */
+	public static function getInstance() {
+		return self::$instance;
+	}
 
-        /**
-         * HungerGames resource data path
-         *
-         * @return string
-         */
-        public function getResourceDataPath(){
-                return $this->dataPath() . "resources/";
-        }
+	/**
+	 * HungerGames base folder
+	 *
+	 * @return string
+	 */
+	public function dataPath() {
+		return $this->getDataFolder();
+	}
 
-        /**
-         * Creates HungerGames resource name
-         *
-         * @param             $resource
-         * @param HungerGames $data
-         */
-        public function createGameResource($resource, HungerGames $data){
-                file_put_contents($this->getResourceDataPath() . $resource . ".dat", serialize($data));
-        }
+	/**
+	 * HungerGames arena folder
+	 *
+	 * @return string
+	 */
+	public function getArenasDataPath() {
+		return $this->dataPath() . "arenas/";
+	}
 
-        /**
-         * Deletes HungerGames resource name
-         *
-         * @param $resource
-         */
-        public function deleteGameResource($resource){
-                if($this->gameResourceExists($resource)){
-                        unlink($this->getResourceDataPath() . $resource . ".dat");
-                }
-        }
+	/**
+	 * HungerGames resource data path
+	 *
+	 * @return string
+	 */
+	public function getResourceDataPath() {
+		return $this->dataPath() . "resources/";
+	}
 
-        /**
-         * Gets game resource by Id
-         *
-         * @param $resource
-         * @return HungerGames|null
-         */
-        public function getGameResource($resource){
-                if(file_exists($this->getResourceDataPath() . $resource . ".dat")){
-                        return unserialize(file_get_contents($this->getResourceDataPath() . $resource . ".dat"));
-                }
-                return null;
-        }
+	/**
+	 * Creates HungerGames resource name
+	 *
+	 * @param	     $resource
+	 * @param HungerGames $data
+	 */
+	public function createGameResource($resource, HungerGames $data) {
+		file_put_contents($this->getResourceDataPath() . $resource . ".dat", serialize($data));
+	}
 
-        /**
-         * Checks if game resource exists
-         *
-         * @param $resource
-         * @return bool
-         */
-        public function gameResourceExists($resource){
-                return file_exists($this->getResourceDataPath() . $resource . ".dat");
-        }
+	/**
+	 * Deletes HungerGames resource name
+	 *
+	 * @param $resource
+	 */
+	public function deleteGameResource($resource) {
+		if ($this->gameResourceExists($resource)) {
+			unlink($this->getResourceDataPath() . $resource . ".dat");
+		}
+	}
 
-        /**
-         * Updates resource by recreating it
-         *
-         * @param             $resource
-         * @param HungerGames $data
-         */
-        public function updateResourceData($resource, HungerGames $data){
-                $this->createGameResource($resource, $data);
-        }
+	/**
+	 * Gets game resource by Id
+	 *
+	 * @param $resource
+	 * @return HungerGames|null
+	 */
+	public function getGameResource($resource) {
+		if (file_exists($this->getResourceDataPath() . $resource . ".dat")) {
+			return unserialize(file_get_contents($this->getResourceDataPath() . $resource . ".dat"));
+		}
+		return null;
+	}
 
-        /**
-         * Gets all games
-         *
-         * @return HungerGames[]|null
-         */
-        public function getAllGameResources(){
-                $data = [];
-                $res = glob($this->getResourceDataPath() . "*", GLOB_BRACE);
-                foreach($res as $ret){
-                        if(substr($ret, strlen($ret) - 4) === ".dat"){
-                                $data[] = unserialize(file_get_contents($ret));
-                        }
-                }
-                return $data === null ? null : $data;
-        }
+	/**
+	 * Checks if game resource exists
+	 *
+	 * @param $resource
+	 * @return bool
+	 */
+	public function gameResourceExists($resource) {
+		return file_exists($this->getResourceDataPath() . $resource . ".dat");
+	}
 
-        /**
-         * Delete HungerGames game arena
-         *
-         * @param HungerGames $game
-         */
-        public function deleteGameArena(HungerGames $game){
-                unlink($this->getArenasDataPath() . $game->getName() . ".yml");
-        }
+	/**
+	 * Updates resource by recreating it
+	 *
+	 * @param	     $resource
+	 * @param HungerGames $data
+	 */
+	public function updateResourceData($resource, HungerGames $data) {
+		$this->createGameResource($resource, $data);
+	}
 
-        /**
-         * Gets all games
-         *
-         * @return Config[]
-         */
-        public function getAllGameArenas(){
-                $data = [];
-                $res = glob($this->getArenasDataPath() . "*.yml", GLOB_BRACE);
-                foreach($res as $ret){
-                        $data[] = new Config($ret, Config::YAML);
-                }
-                return $data === null ? null : $data;
-        }
+	/**
+	 * Gets all games
+	 *
+	 * @return HungerGames[]|null
+	 */
+	public function getAllGameResources() {
+		$data = [];
+		$res = glob($this->getResourceDataPath() . "*", GLOB_BRACE);
+		foreach ($res as $ret) {
+			if (substr($ret, strlen($ret) - 4) === ".dat") {
+				$data[] = unserialize(file_get_contents($ret));
+			}
+		}
+		return $data === null ? null : $data;
+	}
 
-        /**
-         * Checks if game arena exists
-         *
-         * @param $name
-         * @return bool
-         */
-        public function gameArenaExists($name){
-                return file_exists($this->getArenasDataPath() . $name . ".yml");
-        }
+	/**
+	 * Delete HungerGames game arena
+	 *
+	 * @param HungerGames $game
+	 */
+	public function deleteGameArena(HungerGames $game) {
+		unlink($this->getArenasDataPath() . $game->getName() . ".yml");
+	}
 
-        /**
-         * Gets game arena by name
-         *
-         * @param $name
-         * @return null|Config
-         */
-        public function getGameArenaByName($name){
-                return file_exists($this->getArenasDataPath() . $name . ".yml") ? new Config($this->getArenasDataPath() . $name . ".yml", Config::YAML) : null;
-        }
+	/**
+	 * Gets all games
+	 *
+	 * @return Config[]
+	 */
+	public function getAllGameArenas() {
+		$data = [];
+		$res = glob($this->getArenasDataPath() . "*.yml", GLOB_BRACE);
+		foreach ($res as $ret) {
+			$data[] = new Config($ret, Config::YAML);
+		}
+		return $data === null ? null : $data;
+	}
 
-        /**
-         * Game storage, game info is stored
-         *
-         * @return GameStorage
-         */
-        public function getStorage(): GameStorage{
-                return $this->storage;
-        }
+	/**
+	 * Checks if game arena exists
+	 *
+	 * @param $name
+	 * @return bool
+	 */
+	public function gameArenaExists($name) {
+		return file_exists($this->getArenasDataPath() . $name . ".yml");
+	}
 
-        /**
-         * Get global manager, manage all games
-         *
-         * @return GlobalManager
-         */
-        public function getGlobalManager(): GlobalManager{
-                return $this->globalManager;
-        }
+	/**
+	 * Gets game arena by name
+	 *
+	 * @param $name
+	 * @return null|Config
+	 */
+	public function getGameArenaByName($name) {
+		return file_exists($this->getArenasDataPath() . $name . ".yml") ? new Config($this->getArenasDataPath() . $name . ".yml", Config::YAML) : null;
+	}
 
-        /**
-         * Get script manager, manage all scripts
-         *
-         * @return HGAPIScriptManager
-         */
-        public function getScriptManager(): HGAPIScriptManager{
-                return $this->scriptManager;
-        }
+	/**
+	 * Game storage, game info is stored
+	 *
+	 * @return GameStorage
+	 */
+	public function getStorage() : GameStorage {
+		return $this->storage;
+	}
 
-        /**
-         * Gets map backup to backup and reset maps
-         *
-         * @return MapBackup
-         */
-        public function getMapBackup(): MapBackup{
-                return $this->mapBackup;
-        }
+	/**
+	 * Get global manager, manage all games
+	 *
+	 * @return GlobalManager
+	 */
+	public function getGlobalManager() : GlobalManager {
+		return $this->globalManager;
+	}
 
-        /**
-         * Gets sign manager, to refresh all signs
-         *
-         * @return SignManager
-         */
-        public function getSignManager(): SignManager{
-                return $this->signManager;
-        }
+	/**
+	 * Get script manager, manage all scripts
+	 *
+	 * @return HGAPIScriptManager
+	 */
+	public function getScriptManager() : HGAPIScriptManager {
+		return $this->scriptManager;
+	}
 
-        /**
-         * @param string $key
-         * @param string $message
-         */
-        public function pushMessage(string $key, string $message){
-                if(!$this->messages->exists($key)){
-                        $this->messages->set($key, $message);
-                        $this->messages->save();
-                }
-        }
+	/**
+	 * Gets map backup to backup and reset maps
+	 *
+	 * @return MapBackup
+	 */
+	public function getMapBackup() : MapBackup {
+		return $this->mapBackup;
+	}
 
-        /**
-         * Get all the messages
-         *
-         * @return string[]
-         */
-        public function getMessages(){
-                return $this->messages->getAll();
-        }
+	/**
+	 * Gets sign manager, to refresh all signs
+	 *
+	 * @return SignManager
+	 */
+	public function getSignManager() : SignManager {
+		return $this->signManager;
+	}
 
-        /**
-         * Gets message by index
-         *
-         * @param $message
-         * @return string
-         */
-        public function getMessage($message){
-                return $this->getMessages()[$message];
-        }
+	/**
+	 * @param string $key
+	 * @param string $message
+	 */
+	public function pushMessage(string $key, string $message) {
+		if (!$this->messages->exists($key)) {
+			$this->messages->set($key, $message);
+			$this->messages->save();
+		}
+	}
 
-        /**
-         * Create HungerGames game arena
-         *
-         * @param HungerGames $game
-         */
-        public function createGameArena(HungerGames $game){
-                $contents =
-                    [
+	/**
+	 * Get all the messages
+	 *
+	 * @return string[]
+	 */
+	public function getMessages() {
+		return $this->messages->getAll();
+	}
 
-                        "sign_line_1" => "&6-=&e[&fS&cG&e]&6=-",
-                        "sign_line_2" => "&f{on}&f/&a{max}",
-                        "sign_line_3" => "&aGame: &f{game}",
-                        "sign_line_4" => "&eStatus: {status}",
-                        "is_sky_wars" => "no",
-                        "clear_inventory_on_join" => true,
-                        "min_players" => (int)2,
-                        "max_players" => (int)8,
-                        "game_seconds" => (float)60 * 5,
-                        "waiting_seconds" => (float)60,
-                        "game_level" => "world",
-                        "refill_chest_after_seconds" => (float)60 * 2.5,
-                        "chest_items" =>
-                            [
-                                "272 0 1",
-                                "298 0 1",
-                                "299 0 1",
-                                "300 0 1",
-                                "301 0 1"
-                            ],
-                        "lobby_pos" =>
-                            [
-                                "x" => (float)127,
-                                "y" => (float)4,
-                                "z" => (float)128,
-                                "level" => "world"
-                            ],
-                        "death_match_pos" =>
-                            [
-                                "x" => (float)140,
-                                "y" => (float)4,
-                                "z" => (float)150,
-                                "level" => "world"
-                            ],
-                        "slots" => [
-                            "1" =>
-                                [
-                                    "x" => (float)128,
-                                    "y" => (float)4,
-                                    "z" => (float)129,
-                                ],
-                            "2" =>
-                                [
-                                    "x" => (float)138,
-                                    "y" => (float)4,
-                                    "z" => (float)139,
-                                ],
-                        ],
-                        "sign_list" => []
-                    ];
-                $c = new Config($this->getArenasDataPath() . $game->getName() . ".yml", Config::YAML, $contents);
-                $c->save();
-        }
+	/**
+	 * Gets message by index
+	 *
+	 * @param $message
+	 * @return string
+	 */
+	public function getMessage($message) {
+		return $this->getMessages()[$message];
+	}
+
+	/**
+	 * Create HungerGames game arena
+	 *
+	 * @param HungerGames $game
+	 */
+	public function createGameArena(HungerGames $game) {
+		$contents = [
+			"sign_line_1" => "&6-=&e[&fS&cG&e]&6=-",
+			"sign_line_2" => "&f{on}&f/&a{max}",
+			"sign_line_3" => "&aGame: &f{game}",
+			"sign_line_4" => "&eStatus: {status}",
+			"is_sky_wars" => "no",
+			"clear_inventory_on_join" => true,
+			"min_players" => (int) 2,
+			"max_players" => (int) 8,
+			"game_seconds" => (float) 60 * 5,
+			"waiting_seconds" => (float) 60,
+			"game_level" => "world",
+			"refill_chest_after_seconds" => (float) 60 * 2.5,
+			"chest_items" => [
+				"272 0 1",
+				"298 0 1",
+				"299 0 1",
+				"300 0 1",
+				"301 0 1"
+			],
+			"lobby_pos" => [
+				"x" => (float) 127,
+				"y" => (float) 4,
+				"z" => (float) 128,
+				"level" => "world"
+			],
+			"death_match_pos" => [
+				"x" => (float) 140,
+				"y" => (float) 4,
+				"z" => (float) 150,
+				"level" => "world"
+			],
+			"slots" => [
+			    "1" => [
+				    "x" => (float) 128,
+				    "y" => (float) 4,
+				    "z" => (float) 129,
+				],
+			    "2" => [
+				    "x" => (float) 138,
+				    "y" => (float) 4,
+				    "z" => (float) 139,
+				],
+			],
+			"sign_list" => []
+		];
+		$c = new Config($this->getArenasDataPath() . $game->getName() . ".yml", Config::YAML, $contents);
+		$c->save();
+	}
+
 }
