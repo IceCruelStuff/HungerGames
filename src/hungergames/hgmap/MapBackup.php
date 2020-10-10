ae74a7a8af17688fec7cb909f3cee5aa651da39d
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2020-10-10 15:03:01 -0700

    Update MapBackup.php

--- src/hungergames/hgmap/MapBackup.php
+++ src/hungergames/hgmap/MapBackup.php
@@ -1,61 +1,64 @@
 <?php
+
 namespace hungergames\hgmap;
+
 use hungergames\Loader;
 use hungergames\tasks\AsyncMapBackup;
-class MapBackup{
 
-        /** @var Loader */
-        public $loader;
+class MapBackup {
 
-        /**
-         *
-         * MapBackup constructor.
-         *
-         * @param Loader $loader
-         *
-         */
-        public function __construct(Loader $loader){
-                $this->loader = $loader;
-        }
+    /** @var Loader */
+    public $loader;
 
-        /**
-         *
-         * Writes folder backup
-         *
-         * @param        $source
-         * @param        $destination
-         * @param string $game
-         *
-         * @return void
-         */
-        public function write($source, $destination, string $game){
-                $this->asyncWrite($source, $destination, $game);
-        }
+    /**
+     *
+     * MapBackup constructor.
+     *
+     * @param Loader $loader
+     *
+     */
+    public function __construct(Loader $loader) {
+        $this->loader = $loader;
+    }
+
+    /**
+     *
+     * Writes folder backup
+     *
+     * @param    $source
+     * @param    $destination
+     * @param string $game
+     *
+     * @return void
+     */
+    public function write($source, $destination, string $game) {
+        $this->asyncWrite($source, $destination, $game);
+    }
 
 
-        /**
-         *
-         * @param        $source
-         * @param        $destination
-         * @param string $game
-         */
-        public function asyncWrite($source, $destination, string $game){
-                $this->loader->getServer()->getAsyncPool()->submitTask(new AsyncMapBackup($source, $destination, $game));
-        }
+    /**
+     *
+     * @param    $source
+     * @param    $destination
+     * @param string $game
+     */
+    public function asyncWrite($source, $destination, string $game) {
+        $this->loader->getServer()->getAsyncPool()->submitTask(new AsyncMapBackup($source, $destination, $game));
+    }
 
-        /**
-         *
-         * Resets game map
-         *
-         * @param        $source
-         * @param        $destination
-         * @param string $game
-         *
-         * @return void
-         *
-         */
-        public function reset($source, $destination, string $game){
-                $this->asyncWrite($source, $destination, $game);
-        }
+    /**
+     *
+     * Resets game map
+     *
+     * @param    $source
+     * @param    $destination
+     * @param string $game
+     *
+     * @return void
+     *
+     */
+    public function reset($source, $destination, string $game) {
+        $this->asyncWrite($source, $destination, $game);
+    }
 
 }
