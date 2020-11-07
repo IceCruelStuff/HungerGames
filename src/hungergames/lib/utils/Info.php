53573541e0256b62d9abb5a1e0605648ce5ebf8a
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2020-11-06 23:16:39 -0800

    Update Info.php

--- src/hungergames/lib/utils/Info.php
+++ src/hungergames/lib/utils/Info.php
@@ -1,44 +1,54 @@
 <?php
+
 namespace hungergames\lib\utils;
-class Info{
-        const AUTHOR = "xBeastMode";
-        const VERSION = "Build#19.3";
-        const API_VERSION = "0.3";
-        const FOR_API = ["3.0.0"];
-        const CONTRIBUTORS = [];
 
-        /**
-         * @return string
-         */
-        public static function Author(){
-                return Info::AUTHOR;
-        }
+class Info
+{
 
-        /**
-         * @return string
-         */
-        public static function Version(){
-                return Info::VERSION;
-        }
+    const AUTHOR = "xBeastMode";
+    const VERSION = "Build#19.3";
+    const API_VERSION = "0.3";
+    const FOR_API = ["3.0.0"];
+    const CONTRIBUTORS = [];
 
-        /**
-         * @return string
-         */
-        public static function APIVersion(){
-                return Info::API_VERSION;
-        }
+    /**
+     * @return string
+     */
+    public static function Author()
+    {
+        return Info::AUTHOR;
+    }
 
-        /**
-         * @return array
-         */
-        public static function SoftwareAPIs(){
-                return Info::FOR_API;
-        }
+    /**
+     * @return string
+     */
+    public static function Version()
+    {
+        return Info::VERSION;
+    }
 
-        /**
-         * @return array
-         */
-        public static function Contributors(){
-                return Info::CONTRIBUTORS;
-        }
+    /**
+     * @return string
+     */
+    public static function APIVersion()
+    {
+        return Info::API_VERSION;
+    }
+
+    /**
+     * @return array
+     */
+    public static function SoftwareAPIs()
+    {
+        return Info::FOR_API;
+    }
+
+    /**
+     * @return array
+     */
+    public static function Contributors()
+    {
+        return Info::CONTRIBUTORS;
+    }
+
 }
