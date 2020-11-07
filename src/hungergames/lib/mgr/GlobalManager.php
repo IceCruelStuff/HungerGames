7d0d6711ec0f9107f3495b40a75914eaf08dc761
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2020-11-06 23:41:53 -0800

    Update GlobalManager.php

--- src/hungergames/lib/mgr/GlobalManager.php
+++ src/hungergames/lib/mgr/GlobalManager.php
@@ -5,152 +5,167 @@ use hungergames\Loader;
 use hungergames\obj\HungerGames;
 use pocketmine\utils\TextFormat;
 
-class GlobalManager{
-        /** @var HungerGames[] */
-        private $games = [];
-        /** @var GameManager[] */
-        private $gamesMgr = [];
-        /** @var GameEditor[] */
-        private $gamesEditor = [];
-        /** @var Loader */
-        private $HGApi;
+class GlobalManager
+{
 
-        public function __construct(Loader $main){
-                $this->HGApi = $main;
-        }
+    /** @var HungerGames[] */
+    private $games = [];
+    /** @var GameManager[] */
+    private $gamesMgr = [];
+    /** @var GameEditor[] */
+    private $gamesEditor = [];
+    /** @var Loader */
+    private $HGApi;
 
-        /**
-         * Loads game into global manager
-         *
-         * @param HungerGames $game
-         */
-        public function load(HungerGames $game){
-                $game->init();
-                $this->games[$game->getName()] = $game;
-                $game->loadGame($game);
-                $this->HGApi->getStorage()->loadGame($game);
-                $this->gamesMgr[$game->getName()] = new GameManager($game, $this->HGApi);
-                $this->gamesEditor[$game->getName()] = new GameEditor($game);
-                $game->createGameLevelBackup();
-                $this->HGApi->getLogger()->info(TextFormat::GREEN . "Creating map backup for game '" . TextFormat::YELLOW . $game->getName() . TextFormat::GREEN . "'");
-        }
+    public function __construct(Loader $main)
+    {
+        $this->HGApi = $main;
+    }
 
-        /**
-         * Checks if game exists
-         *
-         * @param $resource
-         * @return bool
-         */
-        public function exists($resource){
-                return isset($this->games[$resource]);
-        }
+    /**
+     * Loads game into global manager
+     *
+     * @param HungerGames $game
+     */
+    public function load(HungerGames $game)
+    {
+        $game->init();
+        $this->games[$game->getName()] = $game;
+        $game->loadGame($game);
+        $this->HGApi->getStorage()->loadGame($game);
+        $this->gamesMgr[$game->getName()] = new GameManager($game, $this->HGApi);
+        $this->gamesEditor[$game->getName()] = new GameEditor($game);
+        $game->createGameLevelBackup();
+        $this->HGApi->getLogger()->info(TextFormat::GREEN . "Creating map backup for game '" . TextFormat::YELLOW . $game->getName() . TextFormat::GREEN . "'");
+    }
 
-        /**
-         * Remove game from global manager
-         *
-         * @param HungerGames $game
-         */
-        public function remove(HungerGames $game){
-                if(isset($this->games[$game->getName()])){
-                        unset($this->games[$game->getName()]);
-                }
-                if(isset($this->gamesMgr[$game->getName()])){
-                        unset($this->gamesMgr[$game->getName()]);
-                }
-                if(isset($this->gamesEditor[$game->getName()])){
-                        unset($this->gamesEditor[$game->getName()]);
-                }
-        }
+    /**
+     * Checks if game exists
+     *
+     * @param $resource
+     * @return bool
+     */
+    public function exists($resource)
+    {
+        return isset($this->games[$resource]);
+    }
 
-        /**
-         * Get game by name from global manager
-         *
-         * @param $resource
-         * @return HungerGames|null
-         */
-        public function getGameByName($resource){
-                if(isset($this->games[$resource])){
-                        return $this->games[$resource];
-                }
-                return null;
+    /**
+     * Remove game from global manager
+     *
+     * @param HungerGames $game
+     */
+    public function remove(HungerGames $game)
+    {
+        if (isset($this->games[$game->getName()])) {
+            unset($this->games[$game->getName()]);
         }
+        if (isset($this->gamesMgr[$game->getName()])) {
+            unset($this->gamesMgr[$game->getName()]);
+        }
+        if (isset($this->gamesEditor[$game->getName()])) {
+            unset($this->gamesEditor[$game->getName()]);
+        }
+    }
 
-        /**
-         * Gets game manager by hg object from global manager
-         *
-         * @param HungerGames $game
-         * @return GameManager|null
-         */
-        public function getGameManager(HungerGames $game){
-                if(isset($this->gamesMgr[$game->getName()])){
-                        return $this->gamesMgr[$game->getName()];
-                }
-                return null;
+    /**
+     * Get game by name from global manager
+     *
+     * @param string $resource
+     * @return HungerGames|null
+     */
+    public function getGameByName($resource)
+    {
+        if (isset($this->games[$resource])) {
+            return $this->games[$resource];
         }
+        return null;
+    }
 
-        /**
-         * Get game manager by name from global manager
-         *
-         * @param $resource
-         * @return GameManager|null
-         */
-        public function getGameManagerByName($resource){
-                if(isset($this->gamesMgr[$resource])){
-                        return $this->gamesMgr[$resource];
-                }
-                return null;
+    /**
+     * Gets game manager by hg object from global manager
+     *
+     * @param HungerGames $game
+     * @return GameManager|null
+     */
+    public function getGameManager(HungerGames $game)
+    {
+        if (isset($this->gamesMgr[$game->getName()])) {
+            return $this->gamesMgr[$game->getName()];
         }
+        return null;
+    }
 
-        /**
-         * Gets game editor by hg object from global manager
-         *
-         * @param HungerGames $game
-         * @return null
-         */
-        public function getGameEditor(HungerGames $game){
-                if(isset($this->gamesEditor[$game->getName()])){
-                        return $this->gamesEditor[$game->getName()];
-                }
-                return null;
+    /**
+     * Get game manager by name from global manager
+     *
+     * @param string $resource
+     * @return GameManager|null
+     */
+    public function getGameManagerByName($resource)
+    {
+        if (isset($this->gamesMgr[$resource])) {
+            return $this->gamesMgr[$resource];
         }
+        return null;
+    }
 
-        /**
-         * Get game manager by name from global manager
-         *
-         * @param $resource
-         * @return GameEditor|null
-         */
-        public function getGameEditorByName($resource){
-                if(isset($this->gamesEditor[$resource])){
-                        return $this->gamesEditor[$resource];
-                }
-                return null;
+    /**
+     * Gets game editor by hg object from global manager
+     *
+     * @param HungerGames $game
+     * @return null
+     */
+    public function getGameEditor(HungerGames $game)
+    {
+        if (isset($this->gamesEditor[$game->getName()])) {
+            return $this->gamesEditor[$game->getName()];
         }
+        return null;
+    }
 
-        /**
-         * Gets all loaded games
-         *
-         * @return \hungergames\obj\HungerGames[]
-         */
-        public function getGames(){
-                return $this->games;
+    /**
+     * Get game manager by name from global manager
+     *
+     * @param $resource
+     * @return GameEditor|null
+     */
+    public function getGameEditorByName($resource)
+    {
+        if (isset($this->gamesEditor[$resource])) {
+            return $this->gamesEditor[$resource];
         }
+        return null;
+    }
 
-        /**
-         * Gets all game managers
-         *
-         * @return GameManager[]
-         */
-        public function getGameManagers(){
-                return $this->gamesMgr;
-        }
+    /**
+     * Gets all loaded games
+     *
+     * @return \hungergames\obj\HungerGames[]
+     */
+    public function getGames()
+    {
+        return $this->games;
+    }
 
-        /**
-         * Gets all game editors
-         *
-         * @return \hungergames\lib\editor\GameEditor[]
-         */
-        public function getGameEditors(){
-                return $this->gamesEditor;
-        }
+    /**
+     * Gets all game managers
+     *
+     * @return GameManager[]
+     */
+    public function getGameManagers()
+    {
+        return $this->gamesMgr;
+    }
+
+    /**
+     * Gets all game editors
+     *
+     * @return \hungergames\lib\editor\GameEditor[]
+     */
+    public function getGameEditors()
+    {
+        return $this->gamesEditor;
+    }
+
 }
