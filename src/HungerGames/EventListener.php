dab8128b0d65fcf8f08f8f76d1548793ced7084e
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2021-05-24 18:39:32 -0700

    Update EventListener.php

--- src/HungerGames/EventListener.php
+++ src/HungerGames/EventListener.php
@@ -25,62 +25,62 @@ class EventListener implements Listener {
 	}
 
 	/**
-	 * @param PlayerMoveEvent $e
+	 * @param PlayerMoveEvent $event
 	 */
-	public function onMove(PlayerMoveEvent $e) {
-		$p = $e->getPlayer();
-		$from = clone $e->getFrom();
-		$to = $e->getTo();
-		if ($this->hungerGamesAPI->getStorage()->isPlayerWaiting($p)) {
+	public function onMove(PlayerMoveEvent $event) {
+		$player = $event->getPlayer();
+		$from = clone $event->getFrom();
+		$to = $event->getTo();
+		if ($this->hungerGamesAPI->getStorage()->isPlayerWaiting($player)) {
 			if ($to->x != $from->x || $to->y != $from->y || $to->z != $from->z) {
 				$from->yaw = $to->yaw;
 				$from->pitch = $to->pitch;
-				$e->setTo($from);
+				$event->setTo($from);
 			}
 		}
 	}
 
 	/**
-	 * @param SignChangeEvent $e
+	 * @param SignChangeEvent $event
 	 *
 	 * @return bool|void
 	 *
 	 * @throws \InvalidStateException
 	 */
-	public function onSignChange(SignChangeEvent $e) {
-		$p = $e->getPlayer();
-		if (!$p->hasPermission("hg.sign.create")) {
+	public function onSignChange(SignChangeEvent $event) {
+		$player = $event->getPlayer();
+		if (!$player->hasPermission("hg.sign.create")) {
 			return;
 		}
-		$b = $e->getBlock()->level->getTile($e->getBlock());
-		if ($b instanceof Sign) {
-			$line1 = $e->getLine(0);
-			$line2 = $e->getLine(1);
+		$tile = $event->getBlock()->level->getTile($event->getBlock());
+		if ($tile instanceof Sign) {
+			$line1 = $event->getLine(0);
+			$line2 = $event->getLine(1);
 			if ($line1 === "hg" || $line1 === "[hg]") {
 				if (!$this->hungerGamesAPI->getGlobalManager()->exists($line2)) {
-					$p->sendMessage(Msg::color("&cGame does not exist..."));
+					$player->sendMessage(Msg::color("&cGame does not exist..."));
 					return;
 				}
 				$game = $this->hungerGamesAPI->getGlobalManager()->getGameEditorByName($line2);
-				$game->addSign($b);
-				$p->sendMessage(Msg::color("&aSuccessfully created HG sign!"));
+				$game->addSign($tile);
+				$player->sendMessage(Msg::color("&aSuccessfully created HG sign!"));
 			}
 		}
 	}
 
 	/**
-	 * @param PlayerInteractEvent $e
+	 * @param PlayerInteractEvent $event
 	 */
-	public function onInteract(PlayerInteractEvent $e) {
-		$p = $e->getPlayer();
-		$b = $e->getBlock()->level->getTile($e->getBlock());
-		if ($b instanceof Sign) {
-			if ($this->hungerGamesAPI->getSignManager()->isGameSign($b)) {
-				$game = $this->hungerGamesAPI->getSignManager()->getSignGame($b);
+	public function onInteract(PlayerInteractEvent $event) {
+		$player = $event->getPlayer();
+		$tile = $event->getBlock()->level->getTile($event->getBlock());
+		if ($tile instanceof Sign) {
+			if ($this->hungerGamesAPI->getSignManager()->isGameSign($tile)) {
+				$game = $this->hungerGamesAPI->getSignManager()->getSignGame($tile);
 				if ($game === null) {
 					return;
 				}
-				if ($this->hungerGamesAPI->getStorage()->isPlayerSet($p) || $this->hungerGamesAPI->getStorage()->isPlayerWaiting($p)) {
+				if ($this->hungerGamesAPI->getStorage()->isPlayerSet($player) || $this->hungerGamesAPI->getStorage()->isPlayerWaiting($player)) {
 					return;
 				}
 				if (
@@ -89,81 +89,81 @@ class EventListener implements Listener {
 				) {
 					return;
 				}
-				$this->hungerGamesAPI->getGlobalManager()->getGameManager($game)->addWaitingPlayer($p, true);
+				$this->hungerGamesAPI->getGlobalManager()->getGameManager($game)->addWaitingPlayer($player, true);
 				if ($this->hungerGamesAPI->getGlobalManager()->getGameManager($game)->isWaiting) {
 					return; // checks if task started
 				}
-				$t = new WaitingForPlayersTask($this->hungerGamesAPI, $game);
-				$h = $this->hungerGamesAPI->getScheduler()->scheduleRepeatingTask($t, 20);
-				$t->setHandler($h);
+				$task = new WaitingForPlayersTask($this->hungerGamesAPI, $game);
+				$handler = $this->hungerGamesAPI->getScheduler()->scheduleRepeatingTask($task, 20);
+				$task->setHandler($handler);
 				$this->hungerGamesAPI->getGlobalManager()->getGameManager($game)->isWaiting = true;
 			}
 		}
 	}
 
 	/**
-	 * @param EntityLevelChangeEvent $e
+	 * @param EntityLevelChangeEvent $event
 	 */
-	public function onLevelChange(EntityLevelChangeEvent $e) {
-		$p = $e->getEntity();
-		if ($p instanceof Player) {
-			if ($this->hungerGamesAPI->getStorage()->isPlayerSet($p)) {
-				$game = $this->hungerGamesAPI->getStorage()->getPlayerGame($p);
+	public function onLevelChange(EntityLevelChangeEvent $event) {
+		$player = $event->getEntity();
+		if ($player instanceof Player) {
+			if ($this->hungerGamesAPI->getStorage()->isPlayerSet($player)) {
+				$game = $this->hungerGamesAPI->getStorage()->getPlayerGame($player);
 				if ($game !== null) {
-					$this->hungerGamesAPI->getGlobalManager()->getGameManager($game)->removePlayer($p, true);
+					$this->hungerGamesAPI->getGlobalManager()->getGameManager($game)->removePlayer($player, true);
 				}
-			} elseif ($this->hungerGamesAPI->getStorage()->isPlayerWaiting($p)) {
-				$game = $this->hungerGamesAPI->getStorage()->getWaitingPlayerGame($p);
+			} elseif ($this->hungerGamesAPI->getStorage()->isPlayerWaiting($player)) {
+				$game = $this->hungerGamesAPI->getStorage()->getWaitingPlayerGame($player);
 				if ($game !== null) {
-					$this->hungerGamesAPI->getGlobalManager()->getGameManager($game)->removeWaitingPlayer($p, true);
+					$this->hungerGamesAPI->getGlobalManager()->getGameManager($game)->removeWaitingPlayer($player, true);
 				}
 			}
 		}
 	}
 
 	/**
-	 * @param PlayerQuitEvent $e
+	 * @param PlayerQuitEvent $event
 	 */
-	public function playerQuitEvent(PlayerQuitEvent $e) {
-		$p = $e->getPlayer();
-		if ($this->hungerGamesAPI->getStorage()->isPlayerSet($p)) {
-			$game = $this->hungerGamesAPI->getStorage()->getPlayerGame($p);
+	public function playerQuitEvent(PlayerQuitEvent $event) {
+		$player = $event->getPlayer();
+		if ($this->hungerGamesAPI->getStorage()->isPlayerSet($player)) {
+			$game = $this->hungerGamesAPI->getStorage()->getPlayerGame($player);
 			if ($game !== null) {
-				$this->hungerGamesAPI->getGlobalManager()->getGameManager($game)->removePlayer($p, true);
+				$this->hungerGamesAPI->getGlobalManager()->getGameManager($game)->removePlayer($player, true);
 			}
-		} elseif ($this->hungerGamesAPI->getStorage()->isPlayerWaiting($p)) {
-			$game = $this->hungerGamesAPI->getStorage()->getWaitingPlayerGame($p);
+		} elseif ($this->hungerGamesAPI->getStorage()->isPlayerWaiting($player)) {
+			$game = $this->hungerGamesAPI->getStorage()->getWaitingPlayerGame($player);
 			if ($game !== null) {
-				$this->hungerGamesAPI->getGlobalManager()->getGameManager($game)->removeWaitingPlayer($p, true);
+				$this->hungerGamesAPI->getGlobalManager()->getGameManager($game)->removeWaitingPlayer($player, true);
 			}
 		}
 	}
 
 	/**
-	 * @param PlayerDeathEvent $e
+	 * @param PlayerDeathEvent $event
 	 */
-	public function playerDeathEvent(PlayerDeathEvent $e) {
-		$p = $e->getEntity();
-		if ($this->hungerGamesAPI->getStorage()->isPlayerSet($p)) {
-			$game = $this->hungerGamesAPI->getStorage()->getPlayerGame($p);
+	public function playerDeathEvent(PlayerDeathEvent $event) {
+		$player = $event->getEntity();
+		if ($this->hungerGamesAPI->getStorage()->isPlayerSet($player)) {
+			$game = $this->hungerGamesAPI->getStorage()->getPlayerGame($player);
 			if ($game !== null) {
-				$this->hungerGamesAPI->getGlobalManager()->getGameManager($game)->removePlayer($p);
+				$this->hungerGamesAPI->getGlobalManager()->getGameManager($game)->removePlayer($player);
 			}
 			$count = $this->hungerGamesAPI->getStorage()->getPlayersInGameCount($game);
 			if ($count > 1) {
 				$msg = Msg::getHGMessage("hg.message.death");
-				$msg = str_replace(["%player%", "%game%", "%left%"], [$p->getName(), $game->getName(), $count], $msg);
+				$msg = str_replace(["%player%", "%game%", "%left%"], [$player->getName(), $game->getName(), $count], $msg);
 				$this->hungerGamesAPI->getGlobalManager()->getGameManager($game)->sendGameMessage(Msg::color($msg));
 			}
 		}
 	}
 
 	/**
-	 * @param BlockBreakEvent $e
+	 * @param BlockBreakEvent $event
 	 */
-	public function onBlockBreak(BlockBreakEvent $e) {
-		if ($this->hungerGamesAPI->getStorage()->isPlayerWaiting($e->getPlayer())) {
-			$e->setCancelled();
+	public function onBlockBreak(BlockBreakEvent $event) {
+		if ($this->hungerGamesAPI->getStorage()->isPlayerWaiting($event->getPlayer())) {
+			$event->setCancelled();
 		}
 	}
 
